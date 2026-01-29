b4da7b8562dbafd9248a2636af05413a3f47b443
yalongwastaken <yalong.anthony123@gmail.com>
2026-01-29 18:29:56 -0500

    refactor: migrate from PlatformIO to native ESP-IDF

---
+++ components/pir_sensor/pir.c
@@ -0,0 +1,11 @@
+/**
+ * @file pir.c
+ * @author Anthony Yalong
+ * @brief PIR motion sensor driver implementation
+ */
+
+#include "pir.h"
+#include "esp_log.h"
+
+static const char *TAG = "PIR";
+
---
+++ include/system_config.h
@@ -0,0 +1,19 @@
+// Author: Anthony Yalong
+// Description:
+
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+// imports
+#include <driver/gpio.h>
+#include <esp_err.h>
+#include <esp_log.h>
+
+// pin configuration
+#define PIR_PIN GPIO_NUM_13     // pin 13
+
+// system parameters
+
+// data types
+
+#endif  // SYSTEM_CONFIG_H
